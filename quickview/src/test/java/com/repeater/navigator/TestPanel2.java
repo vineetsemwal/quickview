152cf7eec5e1edee2a4bc9472f781db35c537785
Vineet Semwal <vineetsemwal82@gmail.com>
2012-07-09 14:44:35 +0530

    new files added for a new test getcomponenttag in repeaterutiltest

---
+++ quickview/src/test/java/com/repeater/navigator/TestPanel2.java
@@ -0,0 +1,13 @@
+package com.repeater.navigator;
+
+import com.repeater.TestPanel;
+
+/**
+ * @author Vineet Semwal
+ *
+ */
+public class TestPanel2 extends TestPanel{
+    public TestPanel2(String id){
+        super(id);
+    }
+}
