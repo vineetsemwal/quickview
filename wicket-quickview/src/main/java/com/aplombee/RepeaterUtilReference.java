30f7b347ef29989fdf9d7cccb0fa4d9ae7d60175
vineet <vineetsemwal82@gmail.com>
2012-07-17 11:44:50 +0530

    groupid renamed and following changes

---
+++ wicket-quickview/src/main/java/com/aplombee/RepeaterUtilReference.java
@@ -0,0 +1,19 @@
+package com.aplombee;
+
+import org.apache.wicket.request.resource.PackageResourceReference;
+
+/**
+ * packageresourcereference to repeater.js
+ *
+ * @author Vineet Semwal
+ *
+ */
+public class RepeaterUtilReference extends PackageResourceReference {
+    private static RepeaterUtilReference instance=new RepeaterUtilReference();
+    public static RepeaterUtilReference get(){
+        return instance;
+    }
+    public   RepeaterUtilReference(){
+     super(QuickView.class,"repeater.js");
+    }
+}
